4fc25b78569631541d32051459004a70733d6433
Pietro <donapieppo@yahoo.it>
2016-07-08 11:44:06 +0200

    client code

---
+++ lib/dm_unibo_user_search/client.rb
@@ -0,0 +1,39 @@
+module DmUniboUserSearch
+  class Client
+    CONF_FILE = '/etc/dm_unibo_user_search.yml'
+
+    def initialize
+      return unless File.file?(CONF_FILE)
+
+      conf = YAML.load_file(CONF_FILE)
+      @savon_client = Savon.client do
+        wsdl conf['dsa_search']['wsdl']
+        ssl_verify_mode :none
+        basic_auth [conf['dsa_search']['username'], conf['dsa_search']['password']]
+        convert_request_keys_to :camelcase
+        log_level :warn # :debug
+        # log_level :debug
+        # pretty_print_xml true
+      end
+    end
+
+    def find_user(str)
+      # {:find_state=>"Ok", :count=>"1", :values=>{:user_account_found=>{:user_principal_name=>"name.Surname@unibo.it", :authentication_state=>"NotDefined", :user_state=>"MustChangePassword", :sam_account_name=>"name.Surname", :national_pin=>"NNAPTR00A25F444A", :given_name=>"name", :family_name=>"Surname", :gender=>"M", :birthday=>#<DateTime: 1900-01-01T00:00:00+01:00 ((2440611j,82800s,0n),+3600s,2299161j)>, :birthplace=>"Milano;MI;IT", :employee_id=>"11111", :user_id=>"1111111", :unibo_id=>"111111111-111-1111-af3d-b1d229ef6b15"}}}
+      res = @savon_client.call(:find_user) do 
+        message search_string: str
+      end.body[:find_user_response][:find_user_result]
+      FindResult.new(res)
+    end
+
+    def show_parms
+      res = @savon_client.call(:get_user_parm) do
+        message({})
+      end.body
+    end
+
+    def operations
+      @savon_client.operations
+    end
+  end
+end
+
