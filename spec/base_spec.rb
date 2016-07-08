19bdf271a823c0c016e09aeeb57b7c6e3a088bc9
Pietro <donapieppo@yahoo.it>
2016-07-08 15:20:40 +0200

    common spec

---
+++ spec/base_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require 'savon'
+
+describe DmUniboUserSearch do
+
+  before(:all) do
+    @dsa = DmUniboUserSearch::Client.new
+  end
+
+  it "should list operations" do
+    expect(@dsa.operations).to eq([:get_user_parm, :get_parm_info, :find_user])
+  end
+
+end
+
