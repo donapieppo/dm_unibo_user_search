c1448ac2215785f12cd619fddfd045fe8f0bc7c0
Pietro <donapieppo@yahoo.it>
2021-10-26 11:31:57 +0200

    extrat

---
+++ bin/dm_unibo_user_extract.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require 'dm_unibo_user_search'
+
+name = ARGV.join
+
+puts "in search of #{name}"
+
+p res = DmUniboUserSearch::Client.new.find_user(name)
+
+res.users.each do |user|
+  puts "insert into users set id=#{user.id_anagrafica_unica}, name='#{user.name}', surname='#{user.sn}', staff=1, email='#{user.upn}', contact_email='#{user.upn}'"
+end
+
